125605737d0b54029bed8d16953e1594aae27483
Sydnikat <tonycalifor1@gmail.com>
2018-09-30 22:11:54 +0200

    Adding new function to Calculator

--- BlackJack/src/BlackJack/Entity/Calculator.kt
+++ BlackJack/src/BlackJack/Entity/Calculator.kt
@@ -1,6 +1,7 @@
 package BlackJack.Entity
 
 import BlackJack.Type.CardTypes
+import BlackJack.Type.ResultType
 
 object Calculator {
 
@@ -20,4 +21,38 @@ object Calculator {
         }
         return sum
     }
+
+    fun calculateResult(player: Deck, bank: Deck) : ResultType {
+
+        val playerValue = evaluate(player.cards)
+        val bankValue = evaluate(bank.cards)
+
+        if (playerValue == 21 && player.cards.count() == 2)
+            if (player.cards.any { it.value == CardTypes.JACK } && player.cards.any { it.value == CardTypes.TEN }) return ResultType.WINBYJACK
+
+        if (bankValue == 21 && bank.cards.count() == 2)
+            if (bank.cards.any { it.value == CardTypes.JACK } && bank.cards.any { it.value == CardTypes.TEN }) return ResultType.LOSEBYJACK
+
+
+        when {
+            playerValue > 21 -> return ResultType.LOSE
+            playerValue == 21 -> {
+                return if (bankValue == 21) ResultType.TIE
+                else ResultType.WIN
+            }
+            else -> return when {
+                bankValue == 21 -> ResultType.LOSE
+                bankValue > 21 -> ResultType.WIN
+                else -> {
+                    when {
+                        bankValue < playerValue -> ResultType.WIN
+                        bankValue == playerValue -> ResultType.TIE
+                        else -> ResultType.LOSE
+                    }
+                }
+            }
+        }
+    }
+
+
 }
