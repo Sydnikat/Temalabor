f60fd1ebe57ea3abd00208a63c0a355d1b6fd672
Sydnikat <tonycalifor1@gmail.com>
2018-09-30 22:06:49 +0200

    Making small changes

--- BlackJack/src/BlackJack/Behavior/Behavior.kt
+++ BlackJack/src/BlackJack/Behavior/Behavior.kt
@@ -4,8 +4,8 @@ import BlackJack.Entity.Card
 
 interface Behavior {
     fun hit(cards: ArrayList<Card>) : Boolean
-    fun stand(cards: ArrayList<Card>) : Boolean
     fun double(cards: ArrayList<Card>) : Boolean
     fun split(cards: ArrayList<Card>, numberOfDecks: Int) : Boolean
+    fun getMaximumNumberOfDecks() : Int
 }
 
