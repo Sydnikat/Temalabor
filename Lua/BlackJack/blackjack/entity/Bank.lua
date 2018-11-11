a7f4969275d04f6c2935aee917815eb37a9ea4e7
Szalai Dávid <you@example.com>
2018-11-12 00:18:21 +0100

    Create Bank module.

---
+++ Lua/BlackJack/blackjack/entity/Bank.lua
@@ -0,0 +1,53 @@
+
+local Participant = require "entity.Participant"
+local Deck = require "entity.Deck"
+
+local Bank = {}
+
+setmetatable({}, {
+    __index = Participant,
+    __call = function(class,...)
+        local newBank = class:new(...)
+
+        newBank.deck = Deck({}, 0)
+
+        return newBank
+    end
+})
+
+function Bank:play()
+
+    for _, card in ipairs(self.deck.cards) do
+        card.hidden = false
+    end
+
+    while(self.behavior:hit(self.deck.cards)) do
+
+        table.insert(self.deck.cards, self.dealer:giveCard())
+
+        self.observer:noticeUpdate()
+    end
+
+    if(self.behavior:hit(deck.cards) == false and #slef.deck.cards == 2)
+    then
+        observer.noticeUpdate()
+    end
+end
+
+function Bank:preparation()
+    self.deck.cards = {}
+end
+
+function Bank:receiveFirstCards()
+    table.insert(self.deck.cards, self.dealer:giveCard())
+    local card = self.dealer:giveCard()
+    card.hidden = true
+    table.insert(self.deck.cards, card)
+end
+
+function Bank:showDeck()
+    return self.deck
+end
+
+return Bank
+
