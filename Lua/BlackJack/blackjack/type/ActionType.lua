bf2e74aab1e1deba60c1dbd9913511e7b5701d76
Szalai Dávid <you@example.com>
2018-11-11 18:33:29 +0100

    Create type modules for enum usage.

---
+++ Lua/BlackJack/blackjack/type/ActionType.lua
@@ -0,0 +1,8 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Szalai Dávid
+-- Date: 2018. 11. 11.
+-- Time: 18:30
+-- To change this template use File | Settings | File Templates.
+--
+
---
+++ Lua/BlackJack/blackjack/type/CardType.lua
@@ -0,0 +1,19 @@
+
+local CardType = {
+    TWO = 2,
+    THREE = 3,
+    FOUR = 4,
+    FIVE = 5,
+    SIX = 6,
+    SEVEN = 7,
+    EIGHT = 8,
+    NINE = 9,
+    TEN = 10,
+    JACK = 11,
+    QUEEN = 11,
+    KING = 11,
+    ACE = 11;
+}
+
+return CardType
+
