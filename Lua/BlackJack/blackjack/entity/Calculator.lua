cfef3576596365fa6bc60025f761538d95de2b03
Szalai Dávid <you@example.com>
2018-11-12 01:08:07 +0100

    Create Calculator module.

---
+++ Lua/BlackJack/blackjack/entity/Calculator.lua
@@ -0,0 +1,78 @@
+
+
+local CardType = require "type.CardType"
+local ResultType = require "type.ResultType"
+
+local Calculator = {}
+
+function Calculator:getInstance()
+
+    local evaluate = function(cards)
+        local sum = 0
+
+        for _, card in ipairs(cards) do
+            sum = sum + card.value
+        end
+
+        local numberOfAces = 0
+
+        for _, card in ipairs(cards) do
+            if(card.value == CardType.ACE)
+            then
+                numberOfAces = numberOfAces + 1
+            end
+        end
+
+        while(sum > 21 and numberOfAces > 0) do
+            sum = sum - 10
+            numberOfAces = numberOfAces - 1
+        end
+
+        return sum
+
+    end
+
+    local calculateResult = function(playerDeck, bankDeck)
+
+        local playerValue = evaluate(playerDeck.cards)
+        local bankValue = evaluate(bankDeck.cards)
+
+        if(playerValue == 21 and #playerDeck.cards == 2) then
+            if((playerDeck.cards[1] == CardType.JACK or playerDeck.cards[2] == CardType.JACK) and
+               (playerDeck.cards[1] == CardType.TEN or playerDeck.cards[2] == CardType.TEN)) then
+                return ResultType.WINBYJACK
+            end
+        end
+
+        if(bankValue == 21 and #bankDeck.cards == 2) then
+            if((bankDeck.cards[1] == CardType.JACK or bankDeck.cards[2] == CardType.JACK) and
+               (bankDeck.cards[1] == CardType.TEN or bankDeck.cards[2] == CardType.TEN)) then
+                return ResultType.LOSEBYJACK
+            end
+        end
+
+        if(playerValue > 21) then return ResultType.LOSE
+        elseif(playerValue == 21) then
+            if(bankValue == 21) then return ResultType.TIE
+            else return ResultType.WIN
+            end
+        else
+            if(bankValue == 21) then return ResultType.LOSE
+            elseif(bankValue > 21) then return ResultType.WIN
+            else
+                if(bankValue < playerValue) then return ResultType.WIN
+                elseif(bankValue == playerValue) then return ResultType.TIE
+                else return ResultType.LOSE
+                end
+            end
+        end
+
+    end
+
+    return {
+        evaluate = evaluate,
+        calculateResult = calculateResult
+    }
+end
+
+return Calculator
