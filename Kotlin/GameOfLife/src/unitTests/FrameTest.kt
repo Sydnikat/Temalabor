a710891bbdc550e1433b1a21008f45131826e6d3
Sydnikat <tonycalifor1@gmail.com>
2018-11-24 11:50:15 +0100

    Finish FrameTest unit test.

--- Kotlin/GameOfLife/src/unitTests/FrameTest.kt
+++ Kotlin/GameOfLife/src/unitTests/FrameTest.kt
@@ -69,4 +69,61 @@ class FrameTest {
         assertEquals(StateType.DEAD, frame.cells[1][2].state)
         assertEquals(StateType.DEAD, frame.cells[3][2].state)
     }
+
+    @Test
+    fun checkNextGenForFullHouseTest() {
+
+        System.setIn(ByteArrayInputStream("i\ni\nfullhouse".toByteArray()))
+
+        val frame = FrameMaker.createFrame()
+
+        assertEquals(36, frame.livingCount())
+
+        frame.createNextGen()
+
+        assertEquals(0, frame.livingCount())
+
+    }
+
+    @Test
+    fun checkNextGenForToadTest() {
+        System.setIn(ByteArrayInputStream("i\ni\ntoad".toByteArray()))
+
+        val frame = FrameMaker.createFrame()
+
+        assertEquals(6, frame.livingCount())
+
+        assertEquals(StateType.DEAD,  frame.cells[1][3].state)
+
+        assertEquals(StateType.DEAD,  frame.cells[2][1].state)
+        assertEquals(StateType.ALIVE, frame.cells[2][2].state)
+        assertEquals(StateType.ALIVE, frame.cells[2][3].state)
+        assertEquals(StateType.ALIVE, frame.cells[2][4].state)
+
+        assertEquals(StateType.ALIVE, frame.cells[3][1].state)
+        assertEquals(StateType.ALIVE, frame.cells[3][2].state)
+        assertEquals(StateType.ALIVE, frame.cells[3][3].state)
+        assertEquals(StateType.DEAD,  frame.cells[3][4].state)
+
+        assertEquals(StateType.DEAD,  frame.cells[4][2].state)
+
+
+        frame.createNextGen()
+
+        assertEquals(6, frame.livingCount())
+
+        assertEquals(StateType.ALIVE, frame.cells[1][3].state)
+
+        assertEquals(StateType.ALIVE, frame.cells[2][1].state)
+        assertEquals(StateType.DEAD,  frame.cells[2][2].state)
+        assertEquals(StateType.DEAD,  frame.cells[2][3].state)
+        assertEquals(StateType.ALIVE, frame.cells[2][4].state)
+
+        assertEquals(StateType.ALIVE, frame.cells[3][1].state)
+        assertEquals(StateType.DEAD,  frame.cells[3][2].state)
+        assertEquals(StateType.DEAD,  frame.cells[3][3].state)
+        assertEquals(StateType.ALIVE, frame.cells[3][4].state)
+
+        assertEquals(StateType.ALIVE, frame.cells[4][2].state)
+    }
 }
