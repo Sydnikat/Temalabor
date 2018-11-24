656ceeefe05cb07f2c4cf96edb33e298e5b1a4fd
Sydnikat <tonycalifor1@gmail.com>
2018-11-24 12:12:14 +0100

    Create Unit test module for BlackJack. And create CalculatorTest unit test.

---
+++ Kotlin/BlackJack/src/unitTests/CalculatorTest.kt
@@ -0,0 +1,62 @@
+package unitTests
+
+import org.junit.Test
+
+
+class CalculatorTest {
+
+    @Test
+    fun evaluateCardsWithoutAce() {
+
+    }
+
+    @Test
+    fun evaluateCardsWithAceButLessThan21Test() {
+
+    }
+
+    @Test
+    fun evaluateCardsWithAceButMoreThan21Test() {
+
+    }
+
+    @Test
+    fun evaluateCardsWithMultipleAcesButMoreThan21Test() {
+
+    }
+
+    @Test
+    fun calculateResultWithWinByBlackJackTest() {
+
+    }
+
+    @Test
+    fun calculateResultWithLoseByBlackJackTest() {
+
+    }
+
+    @Test
+    fun calculateResultWith21ButTieTest() {
+
+    }
+
+    @Test
+    fun calculateResultWith21ButNotBlackJackTest() {
+
+    }
+
+    @Test
+    fun calculateResultWithTieTest() {
+
+    }
+
+    @Test
+    fun calculateResultLoseTest() {
+
+    }
+
+    @Test
+    fun calculateWinTest() {
+
+    }
+}
