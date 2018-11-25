155892c1d41f4c77855f88e2391ed5700c6e7ddd
Sydnikat <tonycalifor1@gmail.com>
2018-11-25 01:12:28 +0100

    Simplify ManualInputHandlerTest unit test.

--- Kotlin/GameOfLife/src/unitTests/ManualInputHandlerTest.kt
+++ Kotlin/GameOfLife/src/unitTests/ManualInputHandlerTest.kt
@@ -2,10 +2,8 @@ package unitTests
 
 import gameOfLife.entity.Frame
 import gameOfLife.input.ManualInputHandler
-import gameOfLife.type.GameState
 import org.junit.Test
 
-import org.junit.Assert.*
 import java.io.ByteArrayInputStream
 
 class ManualInputHandlerTest {
@@ -15,34 +13,9 @@ class ManualInputHandlerTest {
 
     @Test
     fun pauseAndContinueTest() {
-        Thread(PausedTest(frame)).start()
+
+        System.setIn(ByteArrayInputStream("\nc\ns".toByteArray()))
         Thread(frame).start() //Elindítom a játékot
         Thread(manualInputHandler).start()
     }
-}
-
-class PausedTest(private val frame: Frame): Runnable{
-    override fun run() {
-
-        Thread.sleep(10)  // Megvárom, hogy mindenképp elinduljon a másik szál
-
-        System.setIn(ByteArrayInputStream("".toByteArray())) // Megállíltom a játékot
-
-        Thread.sleep(10)
-
-        assertEquals(GameState.PAUSED, frame.state)
-
-        System.setIn(ByteArrayInputStream("c".toByteArray())) // Majd elindítom a átékot
-
-        Thread.sleep(10)
-
-        assertEquals(GameState.RUNNING, frame.state)
-
-        System.setIn(ByteArrayInputStream("s".toByteArray())) // Végül leállítom, hogy ne ragadjon be a while ciklus
-
-        Thread.sleep(10)
-
-        assertEquals(GameState.STOP, frame.state)
-    }
-
 }
