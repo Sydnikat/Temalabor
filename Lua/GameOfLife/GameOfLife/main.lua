a9aa7f030fd425680e1719e55193c534d0a981d5
Szalai Dávid <you@example.com>
2018-11-28 19:59:52 +0100

    Override io.read method test.

--- Lua/GameOfLife/GameOfLife/main.lua
+++ Lua/GameOfLife/GameOfLife/main.lua
@@ -1,7 +1,31 @@
 
 
---require "test.unitTests"
+require "test.unitTests"
 
-FrameMaker = require("FrameMaker").getInstance()
 
-FrameMaker.createFrame():simulate()
+local old = io.read
+
+do
+
+    local input = {"y", "y", "toad", 5}
+    local cnt = 0
+
+    io.read = function()
+
+        cnt = cnt+1
+        return input[cnt]
+    end
+
+end
+
+
+
+require("FrameMaker").getInstance().createFrame():simulate()
+
+--FrameMaker.createFrame():simulate()
+
+io.read = old
+
+--local a = io.read()
+
+--print(a)
