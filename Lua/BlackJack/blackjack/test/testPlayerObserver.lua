a9fcaff377f2edb241fae8ad401c74c31a4164b8
Szalai Dávid <you@example.com>
2018-11-30 16:41:35 +0100

    Create testPlayerObserver module for simulating observer without actions.

---
+++ Lua/BlackJack/blackjack/test/testPlayerObserver.lua
@@ -0,0 +1,12 @@
+
+
+local testPlayerObserver = {}
+
+function testPlayerObserver:noticeUpdate() end
+
+function testPlayerObserver:noticeNewGame() end
+
+function testPlayerObserver:noticeEndGame() end
+
+return testPlayerObserver
+
